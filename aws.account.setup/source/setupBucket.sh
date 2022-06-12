60070ca1ccba89242e85b03f25e21ae7e8196365
Your Name <you@example.com>
2022-06-11 21:06:18 -0600

    rename policy file to avoid exclusion in gitignore

--- aws.account.setup/source/setupBucket.sh
+++ aws.account.setup/source/setupBucket.sh
@@ -2,4 +2,4 @@
 # Set up bucket to hold terraform state remotely
 aws s3 mb s3://tfstatebucket999999999999999999999999999999999999
 
-aws s3api put-bucket-policy --bucket tfstatebucket999999999999999999999999999999999999 --policy file://s3bucket.tfstate.json
+aws s3api put-bucket-policy --bucket tfstatebucket999999999999999999999999999999999999 --policy file://s3bucket.remotestate.json
